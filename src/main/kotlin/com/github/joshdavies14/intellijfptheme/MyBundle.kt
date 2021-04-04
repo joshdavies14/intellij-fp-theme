062dacd7e5b5671bd4294aba7c721a4043d97245
GitHub Action <action@github.com>
2021-04-04 16:24:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/joshdavies14/intellijfptheme/MyBundle.kt
+++ src/main/kotlin/com/github/joshdavies14/intellijfptheme/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.joshdavies14.intellijfptheme
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/joshdavies14/intellijfptheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/joshdavies14/intellijfptheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.joshdavies14.intellijfptheme.services
 
+import com.github.joshdavies14.intellijfptheme.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
